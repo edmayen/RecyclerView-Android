45e3cb5dc7bc88cf03d37c77fc3916b384fa77ce
edmayen <ed_mayen@hotmail.com>
2020-06-20 13:54:24 -0500

    First commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.recyclerview
+
+import android.icu.text.CaseMap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var todoList = mutableListOf(
+            Todo("Learn about RecyclerView.", false),
+            Todo("Feed my cat.", false),
+            Todo("Prank my boss.", false),
+            Todo("Eat some curry.", false),
+            Todo("Ask my crush out.", false),
+            Todo("Take a shower.", false)
+        )
+
+        val adapter = TodoAdapter(todoList)
+        rvTodos.adapter = adapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener{
+            val title = etTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+            adapter.notifyItemInserted(todoList.size - 1)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerview
+
+class Todo(
+    val title: String,
+    var isChecked: Boolean
+)
